7d651a711024e7fb4ba282be93f121347f75f4c3
Megan Farley <megfarley4@gmail.com>
2019-01-14 15:43:53 -0600

    setting up redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import SectionLogin from './login.js'
+import { Provider } from 'react-redux'
+import store from './store.js'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-       <h1>Welcome to Itty Bizzy</h1>
-       <SectionLogin />
-     </div>
+      <Provider store={store}>
+        <div>
+          <h1>Welcome to Itty Bizzy</h1>
+          <SectionLogin />
+        </div>
+     </Provider >
     );
   }
 }
---
+++ src/state/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+//import specific reducers
+
+export default combineReducers({
+    //specific reducers called here as key value pairs
+})
