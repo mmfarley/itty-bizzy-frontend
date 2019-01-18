1ea5b8e55abd8aaf1416d074ed04ba0c9dcd56cf
Megan Farley <megfarley4@gmail.com>
2019-01-17 19:06:02 -0600

    navbar updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import history from "../state/history";
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { logout } from '../state/actions/actions'
 
-const styles = {
-    root: {
-        flexGrow: 1,
-    },
-    grow: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20,
-    },
-};
 
-function _NavBar(props) {
-    const { classes } = props;
+class _NavBar extends Component {
+
+    // state = {
+    //     loggedIn: false
+    // }
     
-    const menuOptions = () => {
+    
+    menuOptions = () => {
         if (localStorage.getItem("user")) {
             return(
-            <><Button
-                onClick={() => {
-                    history.push('/my-biz-dash')
-                }}
-                color="inherit">My Itty Biz</Button>
+                <><Button
+                    onClick={() => {
+                        history.push('/my-biz-dash')
+                    }}
+                    color="inherit">My Itty Biz</Button>
                 <Button onClick={() => {
                         history.push('/invoices')
                     }}
@@ -46,39 +34,37 @@ function _NavBar(props) {
                     color="inherit">Messages</Button>
                 <Button
                     onClick={() => {
-                        props.logout()
+                        this.props.logout()
                         history.push('/')
                     }}
                     color="inherit">Logout</Button></>
-            )}else{
+        )}else{
                 return(
-            <><Button
-                onClick={() => {
-                    history.push('/login')
-                }}
-                color="inherit">Login</Button></>
+                <><Button
+                    onClick={() => {
+                        history.push('/login')
+                    }}
+                    color="inherit">Login</Button></>
                 )}
     }
     
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography onClick={() => {
-                        history.push('/user-dash')
-                    }} variant="h5" color="inherit" className={classes.grow}>
-                        Itty Bizzy
-                    </Typography>
-                    {menuOptions()}
-                </Toolbar>
-            </AppBar>
-        </div>
+    render() {
+        return (
+            <div >
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography style={{ flexGrow: 1 }} onClick={() => {
+                            history.push('/user-dash')
+                        }} variant="h5" color="inherit" >
+                            Itty Bizzy
+                        </Typography>
+                        {this.menuOptions()}
+                    </Toolbar>
+                </AppBar>
+            </div>
     );
+    }
 }
 
-_NavBar.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export const NavBar = connect(null, { logout })(withStyles(styles)(_NavBar));
+export const NavBar = connect(null, { logout })(_NavBar);
 
