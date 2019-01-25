b1f65a00e3710ffd560dcf84e7ea34a6793e6e81
Megan Farley <megfarley4@gmail.com>
2019-01-25 13:24:09 -0600

    dev tools put back in

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 const middleware = compose(
     applyMiddleware(thunk),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 const store = createStore(reducer, initialState, middleware)
